ff2049716ecb172b2f6d94e2f86894c0a58d69ba
ganeshkarthick.d@hcl.com <ganeshkarthick.d@hcl.com>
2021-05-15 14:31:48 +0000

    Macros

--- models/staging/stripe/payment_stg.sql
+++ models/staging/stripe/payment_stg.sql
@@ -1,10 +1,14 @@
 with payments as (
 
-    select
-        id as payment_id,
-        orderid as order_id,
-        amount
-    from DBT.stripe.payment
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+from {{ source('stripe','payment') }}
 
 )
 
